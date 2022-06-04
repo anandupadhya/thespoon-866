4cd06acda679df6fb92d47258453e105226a2b1a
Anand Upadhya <anand.upadhya@yahoo.com>
2022-06-04 11:25:22 +0800

    Finish crud actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ - list all restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # CREATE
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+
+  # READ - show a particular restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # UPDATE - update a particular restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # DELETE - delete a particular restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+  # resources :restaurants
 end
